672c04532a6b3ccbefaf45ab3bafc27dd36d6a0c
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-07 21:53:51 +0000

    cart testing

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Visitor navigates to product details page from home page', type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |_n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'See cart' do
+    # ACT
+    visit root_path
+    first('.cart')
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    # expect(page).to have_css 'article.product'
+  end
+end
